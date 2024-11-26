5370ae91aa2d7f6c9ded6305dbdc50da6a2be27d
Alejandro Toshiro Escobedo Correa <toshiro.escobedo@intediperu.com>
2024-11-26 08:53:19 -0500

    go data json

--- example/views.py
+++ example/views.py
@@ -1,16 +1,12 @@
 # example/views.py
 from datetime import datetime
 from django.http import JsonResponse
+import pandas as pd
+
 
 def index(request):
-    # Obtener la fecha y hora actuales
-    now = datetime.now()
-
-    # Formatear la respuesta en JSON
-    data = {
-        "time": now.strftime("%H:%M:%S"),  # Hora en formato HH:MM:SS
-        "day": now.strftime("%A"),  # Día de la semana completo (ejemplo: Monday)
-        "full_date": now.strftime("%Y-%m-%d"),  # Fecha completa en formato YYYY-MM-DD
-    }
+    url = 'https://firebasestorage.googleapis.com/v0/b/react-web-c4127.appspot.com/o/reviews.csv?alt=media&token=c7336dbd-0109-4270-9c39-2967ffbe59a8'
+    df = pd.read_csv(url)
+    data = df.to_dict(orient='records')
 
     return JsonResponse(data)
